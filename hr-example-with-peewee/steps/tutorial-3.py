dba702397a5d522d882c4f2f3880a169283b2c10
stalkcomrade <stanislavpoznyakof@gmail.com>
2019-12-05 16:51:18 +0300

    Add: querying decision

--- hr-example-with-peewee/steps/tutorial-3.py
+++ hr-example-with-peewee/steps/tutorial-3.py
@@ -71,37 +71,52 @@ db_hr.create_tables([HR, Candidate])
 # candidate1 = Candidate.create(reviewed_by=hr1, name="C1", experience=0.6)
 # candidate2 = Candidate.create(reviewed_by=hr2, name="C2", experience=1.6)
 
+## FIXME: rewrite withiut using context global
+
 
 ## first
-@given("the candidate has an experience less than one year")
-def step_impl(context):
-    # Instantiate candidate instance
-    # with an experience given as integer
-    # in years
-    context.hr1 = HR.create(name='J1', experience=0.6)
-    context.candidate1 = Candidate.create(reviewed_by=context.hr1, name="C1", experience=0.6)
+# @given("the candidate has an experience less than one year")
+# def step_impl(context):
+#     # Instantiate candidate instance
+#     # with an experience given as integer
+#     # in years
+#     # context.hr1 = HR.create(name='J1', experience=0.6)
+#     # context.candidate1 = Candidate.create(reviewed_by=context.hr1, name="C1", experience=0.6)
+#     hr1 = HR.create(name='J1', experience=0.6)
+#     candidate1 = Candidate.create(reviewed_by=context.hr1, name="C1", experience=0.6)
 
 
 ## first
 @given("the candidate has an experience less than one year and HR manager has an experience less than one year")
 def step_impl(context):
+    context.cand_name = "J1"
     context.hr1 = HR.create(name='J1', experience=0.6)
     context.candidate1 = Candidate.create(reviewed_by=context.hr1, name="C1", experience=0.6)
+    # hr1 = HR.create(name='hr junior', experience=0.6)
+    # candidate1 = Candidate.create(reviewed_by=context.hr1, name="candidate junior", experience=0.6)
 
     
 ## second
 @given("the candidate has an experience more than one year and HR manager has an experience less than one year")
 def step_impl(context):
-    context.hr1 = HR.create(name='J1', experience=1.6)
-    context.candidate1 = Candidate.create(reviewed_by=context.hr1, name="C1", experience=0.6)
+    context.cand_name = "J2"
+    context.hr1 = HR.create(name='J2', experience=2)
+    context.candidate1 = Candidate.create(reviewed_by=context.hr1, name="C2", experience=0.6)
+    # hr1 = HR.create(name='hr experienced', experience=1.6)
+    # candidate1 = Candidate.create(reviewed_by=context.hr1, name="candidate junior", experience=0.6)
     
 ## third
 @given("the candidate has an experience more than one year and HR manager is experienced")
 def step_impl(context):
-    context.hr1 = HR.create(name='J1', experience=1.6)
-    context.candidate1 = Candidate.create(reviewed_by=context.hr1, name="C1", experience=2.6)
+    context.cand_name = "J3"
+    context.hr1 = HR.create(name='J3', experience=2)
+    context.candidate1 = Candidate.create(reviewed_by=context.hr1, name="C3", experience=2)
+    # hr1 = HR.create(name='hr experienced', experience=1.6)
+    # candidate1 = Candidate.create(reviewed_by=context.hr1, name="candidate experienced", experience=2.6)
 
 
+
+## TODO: update this test
 @when("scanned by HR manager")
 def step_impl(context):
     ## Querying and checking        
@@ -109,14 +124,31 @@ def step_impl(context):
     query = Candidate.update(decision=context.scanning_results).where(Candidate.name == context.candidate1.get_name)
     context.rest = query.execute()
     context.rest is not None
-    
+
+
+## TODO: get results from queries
 @then("the candidate will be definetely hired")
 def step_impl(context):
     # Print the outcome
-    assert (context.scanning_results == "hired")
+    query = (Candidate
+             .select(Candidate, HR)
+             .join(HR)
+             .where(Candidate.name == context.cand_name))
+
+    for item in query:
+        context.scanning_results = item.get_candidate_outcome()
+        assert (context.scanning_results == "hired")
 
 
 @then("the canidate should go to additional interview round")
 def step_impl(context):
     # Print the outcome
-    assert (context.scanning_results == "additional screening")
+        # Print the outcome
+    query = (Candidate
+             .select(Candidate, HR)
+             .join(HR)
+             .where(Candidate.name == context.cand_name))
+
+    for item in query:
+        context.scanning_results = item.get_candidate_outcome()
+        assert (context.scanning_results == "additional screening")
