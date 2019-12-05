d615aebfd87f7c0e6f67671f714f4ee9c0522332
stalkcomrade <stanislavpoznyakof@gmail.com>
2019-12-05 17:04:38 +0300

    Add: separate declaration of models

--- hr-example-with-peewee/steps/tutorial-3.py
+++ hr-example-with-peewee/steps/tutorial-3.py
@@ -1,90 +1,14 @@
 ## INFO: using class inheritance
 
 from behave import *
-from peewee import *
-from playhouse.hybrid import *
-
-db_hr = SqliteDatabase('hr_2.db')
-db_hr.connect()
-
-## INFO: using peewee ORM
-
-class HR(Model):
-    
-    name = CharField()
-    experience = IntegerField()
-    
-    # getter for experience
-    @hybrid_method
-    def get_experience(self):
-        return(self.experience)
-            
-    class Meta:
-        database = db_hr # This model uses the "people.db" database.
 
 
-## update values
-## before doing that, create candidates
-## with status like "pending"
-
-class Candidate(Model):
-    
-    reviewed_by = ForeignKeyField(HR, backref='candidates')
-    
-    name = CharField()
-    experience = IntegerField()
-    decision = CharField(default="pending")
-    
-    @hybrid_property
-    def get_name(self):
-        return(self.name)
-    
-    # getter for experience
-    # FIXME: rewrite using props
-    @hybrid_method
-    def get_experience(self):
-        return(self.experience)
-
-    # Define get outcome for candidate
-    # given information about HR who
-    # is reviewing the candidate 
-    @hybrid_method
-    def get_candidate_outcome(self, hr_person):
-        if ((hr_person.get_experience() < 1) & (self.get_experience() < 1) ):
-            self.decision = "additional screening"
-        elif ((hr_person.get_experience() < 1) & (self.get_experience() > 1) ):
-            self.decision = "hired"
-        else:
-            self.decision = "additional screening"
-            # Return an outcome of the screening
-        return(self.decision)
-    
-    class Meta:
-        database = db_hr # This model uses the "people.db" database.
-
-
-db_hr.create_tables([HR, Candidate])
-
-# hr1 = HR.create(name='J1', experience=0.6)
-# hr2 = HR.create(name='J2', experience=0.7)
-
-# candidate1 = Candidate.create(reviewed_by=hr1, name="C1", experience=0.6)
-# candidate2 = Candidate.create(reviewed_by=hr2, name="C2", experience=1.6)
-
-## FIXME: rewrite withiut using context global
-
-
-## first
-# @given("the candidate has an experience less than one year")
-# def step_impl(context):
-#     # Instantiate candidate instance
-#     # with an experience given as integer
-#     # in years
-#     # context.hr1 = HR.create(name='J1', experience=0.6)
-#     # context.candidate1 = Candidate.create(reviewed_by=context.hr1, name="C1", experience=0.6)
-#     hr1 = HR.create(name='J1', experience=0.6)
-#     candidate1 = Candidate.create(reviewed_by=context.hr1, name="C1", experience=0.6)
-
+## TODO: import
+import sys
+# Add the models folder path to the sys.path list
+sys.path.append('~/arch2019-test/hr-example-with-peewee')
+# Now you can import your module
+from models import *
 
 ## first
 @given("the candidate has an experience less than one year and HR manager has an experience less than one year")
